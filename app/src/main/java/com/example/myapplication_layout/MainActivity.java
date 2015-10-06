cfedb9e89de07f0617c23245bb5c579fecd5a0fe
sangyuKim <catusa159@naver.com>
2015-10-07 01:01:05 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication_layout/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.myapplication_layout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DaumPotActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn = (Button)findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, YoutubeActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn = (Button)findViewById(R.id.button3);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn = (Button)findViewById(R.id.button4);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AlarmActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn = (Button)findViewById(R.id.button5);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MelonActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
